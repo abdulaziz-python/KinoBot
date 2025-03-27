7b242bf224569b0357b5eaa3d0c23eb2aee7239d
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-03-27 13:14:12 +0500

    Update cinema caption to display full description text

--- apps/bot/utils/cinema_caption.py
+++ apps/bot/utils/cinema_caption.py
@@ -41,7 +41,7 @@ def cinema_caption(cinema_id, bot):
 
     text = (
         f"🎬 <b>{_('Name')}:</b> {title}\n\n"
-        f"<blockquote expandable>{description[:600]}</blockquote>\n\n"
+        f"<blockquote expandable>{description}</blockquote>\n\n"
         f"🌐 <b>{_('Country')}:</b> {country}\n"
         f"🚩 <b>{_('Language')}:</b> {language}\n"
         f"🎭 <b>{_('Genre')}:</b> {genres}\n"
